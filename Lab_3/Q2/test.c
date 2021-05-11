6c99e9e6f063f5aa303114e706e800db3f7e9a6a
Adarsh Baghel <adarsh@appointy.com>
2021-05-12 01:35:44 +0530

    added Q2

---
+++ Lab_3/Q2/test.c
@@ -0,0 +1,13 @@
+/* A simple C program for testing our Parenthesis Validation Checker */
+
+#include<stdio.h>
+
+
+int main(){
+    int a = 2;
+    int b = 3;
+    scanf("%d",a);
+    scanf("%d",b);
+    printf("a + b = %d\n", a + b);
+    return (0);
+}
